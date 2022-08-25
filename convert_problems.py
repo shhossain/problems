f182011b8e946d2f344cd0acb4d8471aa9ae0db4
shhossain <hossain0338@gmail.com>
2022-08-25 11:12:47 +0600

    full page screen shot

--- convert_problems.py
+++ convert_problems.py
@@ -11,9 +11,19 @@ CHROMEDRIVER_PATH = 'chromedriver.exe'
 WINDOW_SIZE = "1920,1080"
 
 chrome_options = Options()  
-chrome_options.add_argument("--headless")  
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 # chrome_options.binary_location = CHROME_PATH
+
+# def save_screenshot(driver: webdriver.Chrome, path: str = '/tmp/screenshot.png') -> None:
+#     # Ref: https://stackoverflow.com/a/52572919/
+#     original_size = driver.get_window_size()
+#     required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+#     required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+#     driver.set_window_size(required_width, required_height)
+#     # driver.save_screenshot(path)  # has scrollbar
+#     driver.find_element_by_tag_name('body').screenshot(path)  # avoids scrollbar
+#     driver.set_window_size(original_size['width'], original_size['height'])
 
 def make_screenshot(url, output):
     if not url.startswith('http'):
@@ -21,10 +31,15 @@ def make_screenshot(url, output):
 
     driver = webdriver.Chrome(
         options=chrome_options
-    )  
+    )
     driver.get(url)
-    driver.save_screenshot(output)
+    original_size = driver.get_window_size()
+    required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+    required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+    driver.set_window_size(required_width, required_height)
+    driver.find_element("tag name","body").screenshot(output)
     driver.close()
+
 
 
 def main():
